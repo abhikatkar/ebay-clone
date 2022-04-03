e49bf6ab3b2a7c3f28832961e8dd4ec6ddfd7a69
GobindSetia <gobind.setia@gmail.com>
2022-04-03 16:19:52 +0530

    segrated navbar and footer

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,16 +1,16 @@
 import { Routes,Route } from 'react-router-dom';
-import { Navbar } from '../components/common/Navbar';
+
 import { IndexPage } from '../../IndexPage';
 import { Electronics } from '../components/Electronics/ElectronicsPage';
 import ProductDetails from '../components/Products/ProductDetails';
 import ProductPage from '../components/Products/ProductPage';
 import Login from '../components/Login/Login';
+
  
 
 
 export const AllRoutes = ()=>{
    return <div>
-    <Navbar/>
       <Routes>
         <Route path="/" element={<IndexPage/>}/>
         <Route  path="/Login" element={<Login />} />
